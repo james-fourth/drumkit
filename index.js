adb92a019230a6e6b3fc55c85a2de1eb6e6c5d32
James J Potts IV <james@pottsiv.com>
2019-07-21 12:59:45 -0700

    Unnecessary repetition of switch statement. Dry'ed out this code.

--- index.js
+++ index.js
@@ -1,50 +1,22 @@
 let numberOfButtons = document.querySelectorAll(".drum").length;
-// let buttonLabels = document.querySelectorAll(".drum")
 
-// buttonArray.forEach(function(button) {
-//   button.addEventListener("click", function() { alert("Hello"); });
-// })
 
 for (var i = 0; i < numberOfButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    switch (this.innerHTML) {
-      case "w":
-        let crash = new Audio("sounds/crash.mp3")
-        crash.play();
-        break;
-      case "a":
-        let kick = new Audio("sounds/kick-bass.mp3")
-        kick.play();
-        break;
-      case "s":
-        let snare = new Audio("sounds/snare.mp3")
-        snare.play();
-        break;
-      case "d":
-        let tom1 = new Audio("sounds/tom-1.mp3")
-        tom1.play();
-        break;
-      case "j":
-        let tom2 = new Audio("sounds/tom-2.mp3")
-        tom2.play();
-        break;
-      case "k":
-        let tom3 = new Audio("sounds/tom-3.mp3")
-        tom3.play();
-        break;
-      case "l":
-        let tom4 = new Audio("sounds/tom-4.mp3")
-        tom4.play();
-        break;
-      default:
 
-    }
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
   });
 }
 
-document.addEventListener("keypress", function(event) {
-  switch (event.key) {
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+})
+
+function makeSound(key) {
+  switch (key) {
     case "w":
       let crash = new Audio("sounds/crash.mp3")
       crash.play();
@@ -75,4 +47,4 @@ document.addEventListener("keypress", function(event) {
       break;
     default:
   }
-});
+}
